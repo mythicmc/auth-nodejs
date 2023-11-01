2941d7e2ba6f7b8a4a7f5c6aa7328d59585ed6e9
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2023-11-02 01:33:37 +0530

    Fix typings and gitignore, add dummy auth server

---
+++ src/dummy-service.ts
@@ -0,0 +1,21 @@
+import { Redis } from 'ioredis'
+
+const url = process.argv[2] ?? 'redis://localhost:6379'
+if (!process.argv[2]) {
+  console.log('No URL passed as parameter, using default redis://localhost:6379')
+}
+
+const pub = new Redis(url)
+const sub = new Redis(url)
+
+await sub.psubscribe('mythicauthservice:request:*')
+sub.on('pmessage', (pattern, channel, message) => {
+  const permission = channel.split(':')[2]
+  console.log(`Received auth request for permission ${permission}, authorising: ${message.trim()}`)
+  pub
+    .publish(
+      `mythicauthservice:response:${permission}`,
+      JSON.stringify({ request: message, authorised: true }),
+    )
+    .catch(err => console.error('Error publishing auth response:', err))
+})
